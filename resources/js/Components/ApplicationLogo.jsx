da551c59ef71cced55a86323f5848728927070c9
husam saleh <husamsaleh.eng@gmail.com>
2024-09-23 10:38:37 +0300

    logo added

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,5 +1,5 @@
 export default function ApplicationLogo() {
     return (
-        <img src="/images/logo.png" alt="Application Logo" className="h-10 w-auto" />
+        <img src="/images/husam-shop.png" alt="Application Logo" className="h-20 w-auto" />
     );
 }
